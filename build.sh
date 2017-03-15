6cf09ddcb273d7dd466ea075e3da4290259be329
Alex Kaplan <kaplan2539@gmail.com>
2017-03-14 18:13:54 -0700

    build.sh: added some comments about brew

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#### does not work because mongoose needs to be compiled with =-DMG_ENABLE_HTTP_STREAMING_MULTIPART :
+# on mac, do `brew install mongoose`
+# make sure to havev version 6.7: `brew upgrade mongoose; brew switch mongoose 6.7`
+
+#### so outside buildroot, we do:
 MONGOOSE_URL=https://github.com/cesanta/mongoose/archive/6.7.tar.gz
 MONGOOSE_TAR=${MONGOOSE_URL##*/}
 MONGOOSE_VER=${MONGOOSE_TAR%.tar.gz}
