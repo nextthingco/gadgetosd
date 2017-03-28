0e22f5be7b43893865b784b0701d355bbfdd2298
Wynter Woods <zerotri@gmail.com>
2017-03-28 16:34:12 -0700

    Use cmake --build instead of make for building

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
+BUILD_TYPE="${BUILD_TYPE:-Release}"
 BUILD_DIR="${BUILD_DIR:-build}"
 
 mkdir "${BUILD_DIR}"
 pushd "${BUILD_DIR}"
-cmake ..
-make
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
+cmake --build .
 cpack
+lintian -I gadgetos-cli_0.0.0-1_amd64.deb
 popd
