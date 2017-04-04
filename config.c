606b3a67b9fe5ea7f6697625dab073af92e55d18
Alex Kaplan <kaplan2539@gmail.com>
2017-04-03 23:56:52 -0700

    config.c: initialize(): fallback option for developer mode

--- config.c
+++ config.c
@@ -29,27 +29,37 @@ int   _VERBOSE                     = 0;
 
 int initialize()
 {
+    int ret=0;
+    char *original=0;
+    char *fallback=0;
+
     if(getenv("GADGETOSD_SERVER")) GADGETOSD_SERVER = getenv("GADGETOSD_SERVER");
     if(getenv("GADGETOSD_PORT"))   GADGETOSD_PORT   = getenv("GADGETOSD_PORT");
 
     EXE_PATH = get_exe_path();
+    original = (char*)strdup(EXE_PATH);
 
-    asprintf(&TEMPLATE_PREFIX,"%s/share/gadget/templates",dirname(dirname(EXE_PATH)));
+    asprintf(&TEMPLATE_PREFIX,"%s/share/gadget/templates",dirname(dirname(original)));
     if( access( TEMPLATE_PREFIX, F_OK) == -1 ) {
         // this is a hack to use templates placed next to the gadget binary
         // which is useful for development
-        char *original = TEMPLATE_PREFIX;
-        asprintf(&TEMPLATE_PREFIX,"%s/templates",dirname(EXE_PATH));
+        fallback = (char*)strdup(EXE_PATH);
+        asprintf(&TEMPLATE_PREFIX,"%s/templates",dirname(fallback));
+        //xprint(ERROR,"trying '%s'\n",TEMPLATE_PREFIX);
 
         if( access( TEMPLATE_PREFIX, F_OK) == -1 ) {
-            fprintf(stderr, "gadget init: template prefix '%s' doesn't exist.\n", original);
+            xprint(ERROR, "gadget init: template prefix '%s' doesn't exist.\n", original);
             if(TEMPLATE_PREFIX) free(TEMPLATE_PREFIX);
             TEMPLATE_PREFIX=original;
-            return -1;
+            ret=-1;
+            goto _return;
         }
     }
 
-    return 0;
+_return:
+    if(fallback) free(fallback);
+    if(original) free(original);
+    return ret;
 }
 
 void deinitialize()
