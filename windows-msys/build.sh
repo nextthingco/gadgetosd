c4ad7998129e18bfc0d93fb1237e1c2f94737023
computermouth <computermouth@crunchbangplusplus.org>
2017-04-13 18:21:58 -0700

    Added missing directory in cp commands

--- windows-msys/build.sh
+++ windows-msys/build.sh
@@ -10,9 +10,9 @@ pushd build
 cmake ${src}
 make
 
-cp gadget.exe ${win_build_dir}/sdk-installer
-cp -a ${src}/templates ${win_build_dir}/sdk-installer/
-cp rootfs.tar.gz ${win_build_dir}/sdk-installer/templates/alpine
+cp gadget.exe ${win_build_dir}/gadget-shell/sdk-installer
+cp -a ${src}/templates ${win_build_dir}/gadget-shell/sdk-installer/
+cp rootfs.tar.gz ${win_build_dir}/gadget-shell/sdk-installer/templates/alpine
 
 popd
 
